a0c81de1545c283771a334254065eda1405ae0ff
Teresa Hdz <82226979+Tere-spex@users.noreply.github.com>
2023-10-01 21:02:14 +0200

    form onSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import './App.css'
 import { useMovies } from './customHooks/useMovies'
 import { Movies } from './components/Movies'
+import { useRef } from 'react'
 
 function App() {
-  
   const { movies: mappedMovies } = useMovies()//custom hook
+  const inputRef = useRef()//hago referencia al input
   
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const inputvalue = inputRef.current.value
+    console.log(inputvalue);
+  }
+
   return (
     <div className='page'>
       <header>
         <h1>Buscador Peliculas</h1>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <label htmlFor="">Movie name</label>
-          <input type="text" placeholder='Advengers, Star wars, Lord of the rings' />
+          <input ref={inputRef} type="text" placeholder='Advengers, Star wars, Lord of the rings' />
           <button type='submit'>Buscar</button>
         </form>
         <main>
