50b24523d4708a0423560e4127031320dfeb046f
Teresa Hdz <82226979+Tere-spex@users.noreply.github.com>
2023-10-01 21:57:26 +0200

    new window.FormData(event.target) to get the input value

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css'
 import { useMovies } from './customHooks/useMovies'
 import { Movies } from './components/Movies'
-import { useRef } from 'react'
 
 function App() {
   const { movies: mappedMovies } = useMovies()//custom hook
-  const inputRef = useRef()//hago referencia al input
   
   const handleSubmit = (event) => {
     event.preventDefault()
-    const inputvalue = inputRef.current.value
-    console.log(inputvalue);
+    const data = new window.FormData(event.target)
+    const query = data.get('query')
+    console.log(query);
   }
 
   return (
@@ -19,7 +18,7 @@ function App() {
         <h1>Buscador Peliculas</h1>
         <form onSubmit={handleSubmit}>
           <label htmlFor="">Movie name</label>
-          <input ref={inputRef} type="text" placeholder='Advengers, Star wars, Lord of the rings' />
+          <input name='query' type="text" placeholder='Advengers, Star wars, Lord of the rings' />
           <button type='submit'>Buscar</button>
         </form>
         <main>
