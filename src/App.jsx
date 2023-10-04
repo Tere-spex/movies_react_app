6b151556a2d34af226d91228bb017a2594a79c32
Teresa Hdz <82226979+Tere-spex@users.noreply.github.com>
2023-10-04 11:04:56 +0200

    get data from the inpus using controled form with useState/value/onChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 import './App.css'
 import { useMovies } from './customHooks/useMovies'
 import { Movies } from './components/Movies'
+import { useState } from 'react'
 
 function App() {
   const { movies: mappedMovies } = useMovies()//custom hook
-  
+  const [query, setQuery] = useState()
+
   const handleSubmit = (event) => {
     event.preventDefault()
-    const fields = Object.fromEntries(new window.FormData(event.target))
-    console.log(fields);
+    console.log({query});
+  }
+
+  const handleChange = (event) => {
+    setQuery(event.target.value)
   }
 
   return (
     <div className='page'>
       <header>
         <h1>Buscador Peliculas</h1>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} style={{display:'flex', flexDirection:'column'}} >
           <label htmlFor="">Movie name</label>
-          <input name='input1' type="text" placeholder='Advengers, Star wars, Lord of the rings' />
-          <input name='input2' type="text" placeholder='Advengers, Star wars, Lord of the rings' />
-          <input name='input3' type="text" placeholder='Advengers, Star wars, Lord of the rings' />
-
+          <input onChange={handleChange} value={query} name='input1' type="text" placeholder='Advengers, Star wars, Lord of the rings' />
           <button type='submit'>Buscar</button>
         </form>
         <main>
